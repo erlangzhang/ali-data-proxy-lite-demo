c497fbb3981c61b1f06e5655b9ca186a6c85852d
drmfly <drmfly.liw@gmail.com>
2014-12-05 16:03:19 +0800

    ali-data-proxy demo

---
+++ mockserver.js
@@ -0,0 +1,27 @@
+var express = require( 'express' );
+var app = express();
+
+app.post( '/post', function( req, res ) {
+	var d = '';
+	req.on( 'data', function( chunk ) {
+		d += chunk;
+	} );
+	req.on( 'end', function() {
+		console.log( '-----------' );
+		console.log( d );
+		console.log( '-----------' );
+	} );
+	res.send( 'this is the msg from mockserver!' );
+} );
+
+app.listen( 3001 );
+
+// mock test
+var mocker = require('ali-data-mock');
+//var mocker = require('river-mock');
+var fs = require('fs');
+var rule = fs.readFileSync( './interfaceRules/test.rule.json' );
+
+var schema = JSON.parse( rule );
+console.log( schema );
+console.log( mocker.spec2mock( schema ) );
